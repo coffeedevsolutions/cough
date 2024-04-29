31f81c6a66d932fd1e644323d00daf9a5e7a21e0
Blake Coffee <blake.coffee8@gmail.com>
2024-04-29 18:58:42 -0500

    home page additions as well as mobile media queries

--- cough/src/components/header.js
+++ cough/src/components/header.js
@@ -20,11 +20,45 @@ const Header = () => {
 
             <nav className="navbar">
                 <ul className="nav-list">
-                    <li className="nav-item"><div onClick={() => navigate('/')} className="nav-link">Home</div></li>
-                    <li className="nav-item"><div onClick={() => navigate('/Links')} className="nav-link">Links</div></li>
-                    <li className="nav-item"><div onClick={() => navigate('/Essays')} className="nav-link">Essays</div></li>
-                    <li className="nav-item"><div onClick={() => navigate('/Projects')} className="nav-link">Projects</div></li>
-                    <li className="nav-item"><div onClick={() => navigate('/Taste')} className="nav-link">Taste</div></li>
+                    <div onClick={() => navigate('/')} className="nav-link">Home</div>
+                    <div onClick={() => navigate('/Links')} className="nav-link">Links</div>
+                    <div onClick={() => navigate('/Essays')} className="nav-link">Essays</div>
+                    <div onClick={() => navigate('/Projects')} className="nav-link">Projects</div>
+                    {/* <li className="nav-item"><div onClick={() => navigate('/Taste')} className="nav-link">Taste</div></li> */}
+                </ul>
+            </nav>
+            <nav className="navbar-mobile">
+                <ul className="navlist-mobile">
+                    <img 
+                    src={`${process.env.PUBLIC_URL}/images/HOME.png`} 
+                    alt="Home" 
+                    className="navlink-mobile" 
+                    onClick={() => navigate('/')} 
+                    />
+                    <img 
+                    src={`${process.env.PUBLIC_URL}/images/Links.png`} 
+                    alt="Links" 
+                    className="navlink-mobile" 
+                    onClick={() => navigate('/Links')} 
+                    />
+                    <img 
+                    src={`${process.env.PUBLIC_URL}/images/Essays.png`} 
+                    alt="Essays" 
+                    className="navlink-mobile" 
+                    onClick={() => navigate('/Essays')} 
+                    />
+                    <img 
+                    src={`${process.env.PUBLIC_URL}/images/PROJECTS.png`} 
+                    alt="Projects" 
+                    className="navlink-mobile" 
+                    onClick={() => navigate('/Projects')} 
+                    />
+                    <img 
+                    src={`${process.env.PUBLIC_URL}/images/TASTE.png`} 
+                    alt="Taste" 
+                    className="navlink-mobile" 
+                    onClick={() => navigate('/Taste')} 
+                    />                
                 </ul>
             </nav>
         </header>
